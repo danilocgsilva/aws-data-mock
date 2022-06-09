8136145bac4db1f201ee134ea20b1c71aa0e2dd7
Danilo Silva <danilocgsilva@gmail.com>
2022-06-09 07:50:43 -0300

    Added cost explorer basic response

---
+++ aws_api_mock/CostExplorer.py
@@ -0,0 +1,454 @@
+class CostExplorer:
+    def get_cost_and_usage(self) -> dict:
+        return {
+            "ResultsByTime": [
+                {
+                    "TimePeriod": {
+                        "Start": "2017-05-09",
+                        "End": "2017-05-10"
+                    },
+                    "Total": {
+                        "BlendedCost": {
+                            "Amount": "0.8283195865",
+                            "Unit": "USD"
+                        }
+                    },
+                    "Groups": [],
+                    "Estimated": False
+                },
+                {
+                    "TimePeriod": {
+                        "Start": "2017-05-10",
+                        "End": "2017-05-11"
+                    },
+                    "Total": {
+                        "BlendedCost": {
+                            "Amount": "0.9383288394",
+                            "Unit": "USD"
+                        }
+                    },
+                    "Groups": [],
+                    "Estimated": False
+                },
+                {
+                    "TimePeriod": {
+                        "Start": "2017-05-11",
+                        "End": "2017-05-12"
+                    },
+                    "Total": {
+                        "BlendedCost": {
+                            "Amount": "0.8283300322",
+                            "Unit": "USD"
+                        }
+                    },
+                    "Groups": [],
+                    "Estimated": False
+                },
+                {
+                    "TimePeriod": {
+                        "Start": "2017-05-12",
+                        "End": "2017-05-13"
+                    },
+                    "Total": {
+                        "BlendedCost": {
+                            "Amount": "0.828390103",
+                            "Unit": "USD"
+                        }
+                    },
+                    "Groups": [],
+                    "Estimated": False
+                },
+                {
+                    "TimePeriod": {
+                        "Start": "2017-05-13",
+                        "End": "2017-05-14"
+                    },
+                    "Total": {
+                        "BlendedCost": {
+                            "Amount": "0.8269931709",
+                            "Unit": "USD"
+                        }
+                    },
+                    "Groups": [],
+                    "Estimated": False
+                },
+                {
+                    "TimePeriod": {
+                        "Start": "2017-05-14",
+                        "End": "2017-05-15"
+                    },
+                    "Total": {
+                        "BlendedCost": {
+                            "Amount": "0.938342075",
+                            "Unit": "USD"
+                        }
+                    },
+                    "Groups": [],
+                    "Estimated": False
+                },
+                {
+                    "TimePeriod": {
+                        "Start": "2017-05-15",
+                        "End": "2017-05-16"
+                    },
+                    "Total": {
+                        "BlendedCost": {
+                            "Amount": "0.8283783522",
+                            "Unit": "USD"
+                        }
+                    },
+                    "Groups": [],
+                    "Estimated": False
+                },
+                {
+                    "TimePeriod": {
+                        "Start": "2017-05-16",
+                        "End": "2017-05-17"
+                    },
+                    "Total": {
+                        "BlendedCost": {
+                            "Amount": "0.8283339594",
+                            "Unit": "USD"
+                        }
+                    },
+                    "Groups": [],
+                    "Estimated": False
+                },
+                {
+                    "TimePeriod": {
+                        "Start": "2017-05-17",
+                        "End": "2017-05-18"
+                    },
+                    "Total": {
+                        "BlendedCost": {
+                            "Amount": "0.8283188322",
+                            "Unit": "USD"
+                        }
+                    },
+                    "Groups": [],
+                    "Estimated": False
+                },
+                {
+                    "TimePeriod": {
+                        "Start": "2017-05-18",
+                        "End": "2017-05-19"
+                    },
+                    "Total": {
+                        "BlendedCost": {
+                            "Amount": "0.8283133194",
+                            "Unit": "USD"
+                        }
+                    },
+                    "Groups": [],
+                    "Estimated": False
+                },
+                {
+                    "TimePeriod": {
+                        "Start": "2017-05-19",
+                        "End": "2017-05-20"
+                    },
+                    "Total": {
+                        "BlendedCost": {
+                            "Amount": "0.8283413522",
+                            "Unit": "USD"
+                        }
+                    },
+                    "Groups": [],
+                    "Estimated": False
+                },
+                {
+                    "TimePeriod": {
+                        "Start": "2017-05-20",
+                        "End": "2017-05-21"
+                    },
+                    "Total": {
+                        "BlendedCost": {
+                            "Amount": "0.9887220274",
+                            "Unit": "USD"
+                        }
+                    },
+                    "Groups": [],
+                    "Estimated": False
+                },
+                {
+                    "TimePeriod": {
+                        "Start": "2017-05-21",
+                        "End": "2017-05-22"
+                    },
+                    "Total": {
+                        "BlendedCost": {
+                            "Amount": "0.828304616",
+                            "Unit": "USD"
+                        }
+                    },
+                    "Groups": [],
+                    "Estimated": False
+                },
+                {
+                    "TimePeriod": {
+                        "Start": "2017-05-22",
+                        "End": "2017-05-23"
+                    },
+                    "Total": {
+                        "BlendedCost": {
+                            "Amount": "0.8282905874",
+                            "Unit": "USD"
+                        }
+                    },
+                    "Groups": [],
+                    "Estimated": False
+                },
+                {
+                    "TimePeriod": {
+                        "Start": "2017-05-23",
+                        "End": "2017-05-24"
+                    },
+                    "Total": {
+                        "BlendedCost": {
+                            "Amount": "0.8283010237",
+                            "Unit": "USD"
+                        }
+                    },
+                    "Groups": [],
+                    "Estimated": False
+                },
+                {
+                    "TimePeriod": {
+                        "Start": "2017-05-24",
+                        "End": "2017-05-25"
+                    },
+                    "Total": {
+                        "BlendedCost": {
+                            "Amount": "0.8283433874",
+                            "Unit": "USD"
+                        }
+                    },
+                    "Groups": [],
+                    "Estimated": False
+                },
+                {
+                    "TimePeriod": {
+                        "Start": "2017-05-25",
+                        "End": "2017-05-26"
+                    },
+                    "Total": {
+                        "BlendedCost": {
+                            "Amount": "0.8283099595",
+                            "Unit": "USD"
+                        }
+                    },
+                    "Groups": [],
+                    "Estimated": False
+                },
+                {
+                    "TimePeriod": {
+                        "Start": "2017-05-26",
+                        "End": "2017-05-27"
+                    },
+                    "Total": {
+                        "BlendedCost": {
+                            "Amount": "0.8282988327",
+                            "Unit": "USD"
+                        }
+                    },
+                    "Groups": [],
+                    "Estimated": False
+                },
+                {
+                    "TimePeriod": {
+                        "Start": "2017-05-27",
+                        "End": "2017-05-28"
+                    },
+                    "Total": {
+                        "BlendedCost": {
+                            "Amount": "0.8282984346",
+                            "Unit": "USD"
+                        }
+                    },
+                    "Groups": [],
+                    "Estimated": False
+                },
+                {
+                    "TimePeriod": {
+                        "Start": "2017-05-28",
+                        "End": "2017-05-29"
+                    },
+                    "Total": {
+                        "BlendedCost": {
+                            "Amount": "1.0256233416",
+                            "Unit": "USD"
+                        }
+                    },
+                    "Groups": [],
+                    "Estimated": False
+                },
+                {
+                    "TimePeriod": {
+                        "Start": "2017-05-29",
+                        "End": "2017-05-30"
+                    },
+                    "Total": {
+                        "BlendedCost": {
+                            "Amount": "0.9127400177",
+                            "Unit": "USD"
+                        }
+                    },
+                    "Groups": [],
+                    "Estimated": False
+                },
+                {
+                    "TimePeriod": {
+                        "Start": "2017-05-30",
+                        "End": "2017-05-31"
+                    },
+                    "Total": {
+                        "BlendedCost": {
+                            "Amount": "1.3436859786",
+                            "Unit": "USD"
+                        }
+                    },
+                    "Groups": [],
+                    "Estimated": False
+                },
+                {
+                    "TimePeriod": {
+                        "Start": "2017-05-31",
+                        "End": "2017-06-01"
+                    },
+                    "Total": {
+                        "BlendedCost": {
+                            "Amount": "0.9041322967",
+                            "Unit": "USD"
+                        }
+                    },
+                    "Groups": [],
+                    "Estimated": False
+                },
+                {
+                    "TimePeriod": {
+                        "Start": "2017-06-01",
+                        "End": "2017-06-02"
+                    },
+                    "Total": {
+                        "BlendedCost": {
+                            "Amount": "2.5165489371",
+                            "Unit": "USD"
+                        }
+                    },
+                    "Groups": [],
+                    "Estimated": True
+                },
+                {
+                    "TimePeriod": {
+                        "Start": "2017-06-02",
+                        "End": "2017-06-03"
+                    },
+                    "Total": {
+                        "BlendedCost": {
+                            "Amount": "1.0115540768",
+                            "Unit": "USD"
+                        }
+                    },
+                    "Groups": [],
+                    "Estimated": True
+                },
+                {
+                    "TimePeriod": {
+                        "Start": "2017-06-03",
+                        "End": "2017-06-04"
+                    },
+                    "Total": {
+                        "BlendedCost": {
+                            "Amount": "0.9729120419",
+                            "Unit": "USD"
+                        }
+                    },
+                    "Groups": [],
+                    "Estimated": True
+                },
+                {
+                    "TimePeriod": {
+                        "Start": "2017-06-04",
+                        "End": "2017-06-05"
+                    },
+                    "Total": {
+                        "BlendedCost": {
+                            "Amount": "1.0782099815",
+                            "Unit": "USD"
+                        }
+                    },
+                    "Groups": [],
+                    "Estimated": True
+                },
+                {
+                    "TimePeriod": {
+                        "Start": "2017-06-05",
+                        "End": "2017-06-06"
+                    },
+                    "Total": {
+                        "BlendedCost": {
+                            "Amount": "0.8775415891",
+                            "Unit": "USD"
+                        }
+                    },
+                    "Groups": [],
+                    "Estimated": True
+                },
+                {
+                    "TimePeriod": {
+                        "Start": "2017-06-06",
+                        "End": "2017-06-07"
+                    },
+                    "Total": {
+                        "BlendedCost": {
+                            "Amount": "0.9419058029",
+                            "Unit": "USD"
+                        }
+                    },
+                    "Groups": [],
+                    "Estimated": True
+                },
+                {
+                    "TimePeriod": {
+                        "Start": "2017-06-07",
+                        "End": "2017-06-08"
+                    },
+                    "Total": {
+                        "BlendedCost": {
+                            "Amount": "0.9929764443",
+                            "Unit": "USD"
+                        }
+                    },
+                    "Groups": [],
+                    "Estimated": True
+                },
+                {
+                    "TimePeriod": {
+                        "Start": "2017-06-08",
+                        "End": "2017-06-09"
+                    },
+                    "Total": {
+                        "BlendedCost": {
+                            "Amount": "0.5959950114",
+                            "Unit": "USD"
+                        }
+                    },
+                    "Groups": [],
+                    "Estimated": True
+                }
+            ],
+            "DimensionValueAttributes": [],
+            "ResponseMetadata": {
+                "RequestId": "39cca693-3d86-031b-203c-f197033ba02c",
+                "HTTPStatusCode": 200,
+                "HTTPHeaders": {
+                    "date": "Thu, 09 Jun 2017 10:47:16 GMT",
+                    "content-type": "application/x-amz-json-1.1",
+                    "content-length": "4688",
+                    "connection": "keep-alive",
+                    "x-amzn-requestid": "39cca693-3d86-031b-203c-f197033ba02c",
+                    "cache-control": "no-cache"
+                },
+                "RetryAttempts": 0
+            }
+        }
